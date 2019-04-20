7a06bbe3eec49885451b53e7819fdde276231287
Ashwin Singh <ashwinsinghgr8@gmail.com>
2019-04-20 18:17:09 +0530

    Updates

---
+++ precache-manifest.13c96d592954a3c6912870f31709614c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0c7a2bbd99c646f2c1c",
+    "url": "/reactify/static/css/main.17959f3b.chunk.css"
+  },
+  {
+    "revision": "b0c7a2bbd99c646f2c1c",
+    "url": "/reactify/static/js/main.79be8d6b.chunk.js"
+  },
+  {
+    "revision": "fffc0ed3d49eaa90f927",
+    "url": "/reactify/static/js/runtime~main.c48bb6c0.js"
+  },
+  {
+    "revision": "47d9c5ba479ff8a9be27",
+    "url": "/reactify/static/js/2.3700e223.chunk.js"
+  },
+  {
+    "revision": "bf935a042c2b1fed30c3131f6b3dcfc6",
+    "url": "/reactify/index.html"
+  }
+];
