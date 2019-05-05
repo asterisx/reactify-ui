bdddabaa776ba4ee11161e56d6ada5303a58233b
Ashwin Singh <ashwinsinghgr8@gmail.com>
2019-05-05 13:22:49 +0530

    Updates

--- precache-manifest.99f22e0f93f13b742d0d8f313d753e63.js
+++ precache-manifest.99f22e0f93f13b742d0d8f313d753e63.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d48758396afe2fa99709",
+    "revision": "e3f19bab651c01cfc5e1",
     "url": "/reactify/static/css/main.02bde342.chunk.css"
   },
   {
-    "revision": "d48758396afe2fa99709",
-    "url": "/reactify/static/js/main.755f3b89.chunk.js"
+    "revision": "e3f19bab651c01cfc5e1",
+    "url": "/reactify/static/js/main.0d95db70.chunk.js"
   },
   {
     "revision": "fffc0ed3d49eaa90f927",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/reactify/static/js/2.5a4a64aa.chunk.js"
   },
   {
-    "revision": "f4ea8f58d3fb8e16cf73453240952330",
+    "revision": "d51870bf9fb2753e7eed62d340c0a0b8",
     "url": "/reactify/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactify/precache-manifest.b25366b840c53cce483104adb9334153.js"
+  "/reactify/precache-manifest.99f22e0f93f13b742d0d8f313d753e63.js"
 );
 
 workbox.clientsClaim();
