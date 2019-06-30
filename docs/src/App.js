c63ad41735393f2b5892deba554ec15cdff2c2bd
Ashwin Singh <ashwinsinghgr8@gmail.com>
2019-06-30 13:24:24 +0530

    Temproary fix for docs routing issue

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import Octicon, { MarkGithub } from '@githubprimer/octicons-react';
 import './App.scss';
 import {
