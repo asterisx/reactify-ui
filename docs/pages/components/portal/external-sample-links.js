1288be313c6b7ba8a038013fda26e5e911f9adcb
Ashwin Singh <ashwinsinghgr8@gmail.com>
2019-07-15 23:22:50 +0530

    Updated portal stackblitz links

--- docs/pages/components/portal/external-sample-links.js
+++ docs/pages/components/portal/external-sample-links.js
@@ -1,8 +1,8 @@
-const autoCloseLink = 'https://stackblitz.com/edit/reactify-ui-class-portal-auto-close';
-const hideBackdropLink = 'https://stackblitz.com/edit/reactify-ui-class-portal-hide-backdrop';
-const onCloseLink = 'https://stackblitz.com/edit/reactify-ui-class-portal-close-event';
-const portalLocationLink = 'https://stackblitz.com/edit/reactify-ui-class-portal-locations';
-const simpleLink = 'https://stackblitz.com/edit/reactify-ui-class-portal-simple';
+const autoCloseLink = 'https://stackblitz.com/edit/reactify-ui-portal-auto-close';
+const hideBackdropLink = 'https://stackblitz.com/edit/reactify-ui-portal-hide-backdrop';
+const onCloseLink = 'https://stackblitz.com/edit/reactify-ui-portal-close-event';
+const portalLocationLink = 'https://stackblitz.com/edit/reactify-ui-portal-locations';
+const simpleLink = 'https://stackblitz.com/edit/reactify-ui-portal-simple';
 
 export {
   autoCloseLink,
